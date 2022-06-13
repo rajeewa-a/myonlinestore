babcc75a2460026cd35646d898c04a5c9a2bc9ac
Rajeewa <rajeewa.asmadala01@gmail.com>
2022-06-13 14:56:03 +0530

    product gallery

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from audioop import avg
 import imp
 from itertools import count, product
+from tabnanny import verbose
 from django.db import models
 from accounts.models import Account
 from category.models import Category
@@ -83,3 +84,15 @@ class ReviewRating(models.Model):
 
     def __str__(self):
         return self.subject
+
+
+class ProductGallery(models.Model):
+    product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='store/products', max_length=255)
+
+    def __str__(self):
+        return self.product.product_name
+
+    class Meta:
+        verbose_name = 'productgallery'
+        verbose_name_plural = 'product gallery'
--- store/views.py
+++ store/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render,get_object_or_404, redirect
 from orders.models import OrderProduct
 
 from store.forms import ReviewForm
-from .models import Product, ReviewRating
+from .models import Product, ProductGallery, ReviewRating
 from category.models import Category
 from carts.views import _cart_id
 from carts.models import Cart,CartItem
@@ -60,11 +60,15 @@ def product_detail(request,category_slug,product_slug):
 
     reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
 
+    product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+
+
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
         'orderproduct': orderproduct,
         'reviews': reviews,
+        'product_gallery': product_gallery,
     }
 
 
